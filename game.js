c92225584dc8be5c9543747364876a1b4b9958df
motoko111 <kobo1853@yahoo.co.jp>
2023-11-04 17:58:31 +0900

    更新 ratina対応

--- game.js
+++ game.js
@@ -289,6 +289,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"dc418866-87fb-4b70-9117-4c0223025516","remote_package_size":11826747,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":11826747,"audio":false}]});
+  loadPackage({"package_uuid":"1911c921-0f45-400e-ad6e-eb365e44ea68","remote_package_size":11826759,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":11826759,"audio":false}]});
 
 })();
